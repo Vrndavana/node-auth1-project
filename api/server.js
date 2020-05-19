50795cf5bca050aac89b4c9fbffc4bb0713c17d9
Vrndavana <Vrmdavama.th@gmail.com>
2020-05-18 20:00:10 -0700

    Cookies Setup

--- api/server.js
+++ api/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const server = express();
+const cors = require("cors");
 
+server.use(session(sessionConfig)); 
+server.use(helmet());
 server.use(express.json());
+server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
+server.use(cors());
 
 server.get("/", (req, res) => {
   res.json({ api: "*Root Server Running*" });
@@ -11,25 +17,16 @@ server.get("/", (req, res) => {
 // one hour in milliseconds
 // send the cookie only over https, true in production -> secure || true
 // true means client JS cannot access the cookie ----------------
-
-// const sessionConfig = {
-//   cookie: {
-//     maxAge: 1000 * 60 * 60,
-//     secure: process.env.SECURE_COOKIE || false, 
-//   },
-//   resave: false,
-//   saveUninitialized: process.env.USER_ALLOWED_COOKIES || true,
-//   name: "monster",
-//   secret: process.env.COOKIE_SECRET || "keepitsecret,keepitsafe!",
-// };
+const sessionConfig = {
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: process.env.SECURE_COOKIE || false, 
+  },
+  resave: false,
+  saveUninitialized: process.env.USER_ALLOWED_COOKIES || true,
+  name: "secretadmin",
+  secret: process.env.COOKIE_SECRET || "keepitsecret,keepitsafe!",
+};
 
 // turn on sessions for the API ---------
-
-// server.use(session(sessionConfig)); 
-// server.use(helmet());
-// server.use(express.json());
-// server.use("/api/users", usersRouter);
-// server.use("/api/auth", authRouter);
-// const server = express();
-
 module.exports = server;
---
+++ data/seeds/002-unit.js
@@ -0,0 +1,32 @@
+exports.seed = function (knex) {
+    // 000-cleanup.js already cleaned out all tables
+  
+    const users = [
+      {
+        username: "groot",
+        password: "Iamgroot!",
+        role: 1,
+      },
+      {
+        username: "admin",
+        password: "keepitsecret",
+        role: 1,
+      },
+      {
+        username: "me",
+        password: "changethepass",
+        role: 2,
+      },
+      {
+        username: "nobody",
+        password: "hasnorole",
+      },
+      {
+        username: "notme",
+        password: "hasnorole",
+      },
+    ];
+  
+    return knex("users").insert(users);
+  };
+  
