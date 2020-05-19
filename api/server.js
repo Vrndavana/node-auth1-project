15e89b646cfa45cb877341fe30daca5bc0e561a2
Vrndavana <Vrmdavama.th@gmail.com>
2020-05-18 18:57:30 -0700

    Building Users

--- api/server.js
+++ api/server.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const server = express();
 
-
 server.use(express.json());
 
 server.get("/", (req, res) => {
   res.json({ api: "*Root Server Running*" });
 });
+
+// Create The Logic for Your Cookie 
+// one hour in milliseconds
+// send the cookie only over https, true in production -> secure || true
+// true means client JS cannot access the cookie ----------------
+
+// const sessionConfig = {
+//   cookie: {
+//     maxAge: 1000 * 60 * 60,
+//     secure: process.env.SECURE_COOKIE || false, 
+//   },
+//   resave: false,
+//   saveUninitialized: process.env.USER_ALLOWED_COOKIES || true,
+//   name: "monster",
+//   secret: process.env.COOKIE_SECRET || "keepitsecret,keepitsafe!",
+// };
+
+// turn on sessions for the API ---------
+
+// server.use(session(sessionConfig)); 
+// server.use(helmet());
+// server.use(express.json());
+// server.use("/api/users", usersRouter);
+// server.use("/api/auth", authRouter);
+// const server = express();
 
 module.exports = server;
--- knexfile.js
+++ knexfile.js
@@ -18,20 +18,4 @@ module.exports = {
         directory: "./data/seeds",
       },
   },
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
